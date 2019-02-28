3b19c0eadabe8b994e880382888a8fd69f2fbe26
Walmyr Filho <limaesilva@qelp.com>
2019-02-28 04:27:53 +0100

    Update where specs are found on default protractor config file

--- tests/createProtractorConfig.js
+++ tests/createProtractorConfig.js
@@ -1,7 +1,7 @@
 module.exports = providedConfig => {
   const defaultConfig = {
     seleniumAddress: "http://localhost:4444/wd/hub",
-    specs: ["../specs/spec.js"]
+    specs: ["../specs/*.spec.js"]
   };
 
   return Object.assign({}, defaultConfig, providedConfig);
